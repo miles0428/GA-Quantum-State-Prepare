809e079c8e1ecc916be0d424584498e92db8ace0
miles0428 <tearofclown0428@gmail.com>
2023-09-11 20:20:01 +0800

    add function GA(),make GA.py callable

--- GA.py
+++ GA.py
@@ -21,13 +21,15 @@ import multiprocessing as mp
 from qiskit_algorithms import optimizers
 import os
 from gene import Gene_Circuit
+from functools import partial
 
-def get_prob_distribution(circuit : qk.QuantumCircuit, theta : list|np.ndarray) -> np.ndarray:
+def get_prob_distribution(circuit : qk.QuantumCircuit, theta : list|np.ndarray, num_qubits :int) -> np.ndarray:
     '''
     Get the probability distribution of a circuit
     Args:
         circuit: a quantum circuit
         theta: a list of theta
+        num_qubits: number of qubits
     Returns:
         prob_distribution: the probability distribution of the circuit
     '''
@@ -37,7 +39,7 @@ def get_prob_distribution(circuit : qk.QuantumCircuit, theta : list|np.ndarray) 
     job = qk.execute(circuit, backend, shots=1000)
     result = job.result()
     counts = result.get_counts()
-    prob_distribution = np.zeros(2**num_qubit)
+    prob_distribution = np.zeros(2**num_qubits)
     for key in counts.keys():
         prob_distribution[int(key, 2)] = counts[key]/1000
     return prob_distribution
@@ -71,12 +73,16 @@ def statevector(Gene : Gene_Circuit, theta : np.ndarray, backend : qk.providers.
     statevector = result.get_statevector()
     return statevector
 
-def get_optimized_fidelity(Gene : Gene_Circuit, target_statevector:np.ndarray) -> (float, int, np.ndarray):
+def get_optimized_fidelity(Gene : Gene_Circuit, target_statevector:np.ndarray ,**kwargs) -> (float, int, np.ndarray):
     '''
     Get the optimized fidelity of a gene
     Args:
         Gene: Gene_Circuit
-        num_qubit: number of qubits
+        target_statevector: the target statevector
+    
+    kwargs:
+        optimizer: the optimizer of the circuit. Default: optimizers.SPSA(maxiter=1000)
+
     Returns:
         fidelity: the optimized fidelity of the gene
         depth: the depth of the circuit
@@ -85,6 +91,10 @@ def get_optimized_fidelity(Gene : Gene_Circuit, target_statevector:np.ndarray) -
     backend = qk.Aer.get_backend('statevector_simulator')
     num_parameters = Gene.num_parameters
     theta = np.random.rand(num_parameters)
+    try:
+        optimizer = kwargs['optimizer']
+    except:
+        optimizer = optimizers.SPSA(maxiter=1000)
     #define the loss function
     def loss(theta):
         fidelity = get_fidelity(statevector(Gene, theta, backend), target_statevector)
@@ -97,43 +107,91 @@ def get_optimized_fidelity(Gene : Gene_Circuit, target_statevector:np.ndarray) -
     depth=Gene.depth()
     return fidelity,depth,theta.x
 
-def get_fidelity_depth(gene : list) -> (float, int, np.ndarray):
+def get_fidelity_depth(gene : list, **kwargs ) -> (float, int, np.ndarray):
     '''
     this function is used to get the fidelity and depth of a gene
     Args:
         gene: a list of 0-10
+
+    kwargs:
+        num_qubit: number of qubits
+        target_statevector: the target statevector
+        optimizer: the optimizer of the circuit. Default: optimizers.SPSA(maxiter=1000)
+
     Returns:
         fidelity: the fidelity of the gene
         depth: the depth of the circuit
         theta: the optimized theta
     '''
+    try:
+        num_qubit = kwargs['num_qubit']
+    except:
+        raise Exception('num_qubit is not defined')
+    try:
+        target_statevector = kwargs['target_statevector']
+    except:
+        raise Exception('target_statevector is not defined')
+    try:
+        optimizer = kwargs['optimizer']
+    except:
+        optimizer = optimizers.SPSA(maxiter=1000)
+    
     Gene = Gene_Circuit(gene, num_qubit)
     # print(gene)
-    fidelity,depth,theta = get_optimized_fidelity(Gene, target_statevector)
+    fidelity,depth,theta = get_optimized_fidelity(Gene, target_statevector,optimizer=optimizer)
     # print(theta)
     # print(statevector(Gene, theta, qk.Aer.get_backend('statevector_simulator')), target_statevector)
     # print(fidelity)
     return fidelity,depth,theta
 
-num_genes = 20
-length_gene = 10
-random_gene = np.random.randint(0,11,num_genes*length_gene).reshape(num_genes,length_gene)
-mutation_rate = 0.1
-#calculate the fidelity and depth for each gene
-#use multiprocessing to speed up
-path = 'data'
-expriement = 'test'
-os.makedirs(f'{path}/{expriement}',exist_ok=True)
-optimizer = optimizers.SPSA(maxiter=1000)
-num_qubit = 3
-target_statevector = np.array([0.7, 0.7,0,0,0,0,0,0])
-iter = 30
-if __name__ == '__main__':
+#rewrite the GA function
+def GA(target_statevector : np.ndarray ,num_qubit : int ,**kwargs):
+    '''
+    this function is used to implement the genetic algorithm on the quantum circuit for preparing the target statevector
+    Args:
+        target_statevector: the target statevector
+        num_qubit: number of qubits
+    kwargs:
+        num_genes: number of genes. Default: 20
+        length_gene: length of gene. Default: 10
+        mutation_rate: the mutation rate. Default: 0.1
+        cpu_count: the number of cpu used. Default: mp.cpu_count()
+        path: the path to save the result. Default: data
+        expriement: the name of the expriement. Default: test
+        optimizer: the optimizer of the circuit. Default: optimizers.SPSA(maxiter=1000)
+        iter: the number of iteration. Default: 30
+    Returns:
+        None
+    '''
+    kwargs_default = {'num_genes':20,
+               'length_gene':10,
+               'mutation_rate':0.1,
+               'cpu_count':mp.cpu_count(),
+               'path':'data',
+               'expriement':'test',
+               'optimizer':optimizers.SPSA(maxiter=1000),
+               'iter':30}
+    for key in kwargs_default.keys():
+        if key not in kwargs.keys():
+            kwargs[key] = kwargs_default[key]
+    num_genes = kwargs['num_genes']
+    length_gene = kwargs['length_gene']
+    mutation_rate = kwargs['mutation_rate']
+    cpu_count = kwargs['cpu_count']
+    path = kwargs['path']
+    expriement = kwargs['expriement']
+    optimizer = kwargs['optimizer']
+    iter = kwargs['iter']
+    random_gene = np.random.randint(0,11,num_genes*length_gene).reshape(num_genes,length_gene)
+    partial_get_fidelity_depth = partial(get_fidelity_depth,
+                                         num_qubit=num_qubit, 
+                                         target_statevector=target_statevector, 
+                                         optimizer=optimizer)
     for i in range(iter):
         #use multiprocessing to speed up
-        pool = mp.Pool(mp.cpu_count())
-        result = pool.map(get_fidelity_depth, random_gene)
-        #mkdir 1st_generation
+        pool = mp.Pool(cpu_count)
+        result = pool.map(partial_get_fidelity_depth, random_gene)
+        #mkdir ist_generation
         result=np.array(result,dtype=object)
         pool.close()
         os.makedirs(f'{path}/{expriement}/{i}st_generation',exist_ok=True)
@@ -141,21 +199,29 @@ if __name__ == '__main__':
         np.save(f'{path}/{expriement}/{i}st_generation/result.npy', result)
         #save the random gene
         np.save(f'{path}/{expriement}/{i}st_generation/random_gene.npy', random_gene)
-
         #load the result
         result = np.load(f'{path}/{expriement}/{i}st_generation/result.npy', allow_pickle=True)
 
-        ii = 0
+        ii = 0 #the fidelity threshold
         while (True and ii<100):
             # find the gene with the fidelity larger than 0.99
             gene = result[:,0]>0.99 - 0.01*ii
             ii += 1
             if np.sum(gene)>=6:
                 break
-
+            elif 0.99 - 0.01*ii<0.9:
+                if np.sum(gene)>3:
+                    break
+                elif np.sum(gene)>0:
+                    #randomly choose 2 genes
+                    gene[np.random.randint(0,len(gene))]=True
+                    gene[np.random.randint(0,len(gene))]=True
+                    break
+                else:
+                    raise Exception('No gene with fidelity larger than 0.90')
+        
         index=np.array([]).astype(int)
         #get the index of 10 genes with the smallest depth and fidelity larger than 0.99
-
         for j in np.argsort(result[:,1]):
             if gene[j]:
                 index=np.append(index,j)
@@ -167,7 +233,6 @@ if __name__ == '__main__':
         #save the 10 genes
         np.save(f'{path}/{expriement}/{i}st_generation/10_smallest_depth_gene.npy', random_gene[index])
         np.save(f'{path}/{expriement}/{i}st_generation/10_smallest_depth_result.npy', result[index])
-        
         parent_gene = random_gene[index]
         #use parent gene to generate child gene
         child_gene = np.zeros((num_genes,length_gene)).astype(int)
@@ -184,10 +249,11 @@ if __name__ == '__main__':
             for k in range(length_gene):
                 if np.random.rand()<mutation_rate:
                     child_gene[j][k]=np.random.randint(0,11)
-        
-        #randomly generate 10 genes
+        #randomly generate 10% genes
         child_gene[num_genes-int(num_genes/10):] = np.random.randint(0,11,int(num_genes/10)*length_gene).reshape(int(num_genes/10),length_gene)
         #add the 10 genes with the smallest depth
         child_gene[num_genes-int(num_genes/10)-len(index):num_genes-int(num_genes/10)] = random_gene[index]
         random_gene = child_gene.astype(int)
 
+if __name__ == '__main__':
+    GA(np.array([1,0,0,0,0,0,0,-1])/np.sqrt(2), 3, iter=3, expriement='test')
